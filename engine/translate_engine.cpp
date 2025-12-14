1a701d6736b3a4ffa90e35fb7cf47bec5c2bbc2e
kavos113 <kohei.hadano@gmail.com>
2025-12-14 14:40:35 +0900

    translate engine ok

--- engine/translate_engine.cpp
+++ engine/translate_engine.cpp
@@ -1,1 +1,185 @@
 #include "translate_engine.h"
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+translate_engine::~translate_engine()
+{
+    if (m_model != nullptr)
+    {
+        free_model();
+    }
+}
+
+bool translate_engine::load_model()
+{
+    ggml_backend_load_all();
+
+    llama_model_params model_params = llama_model_default_params();
+    model_params.n_gpu_layers = N_GPU_LAYERS;
+
+    m_model = llama_model_load_from_file(MODEL_PATH.c_str(), model_params);
+    if (m_model == nullptr)
+    {
+        std::cerr << "failed to load model" << std::endl;
+        return false;
+    }
+    
+    llama_sampler_chain_params sampler_params = llama_sampler_chain_default_params();
+    sampler_params.no_perf = false;
+    
+    m_sampler = llama_sampler_chain_init(sampler_params);
+    if (m_sampler != nullptr)
+    {
+        std::cerr << "failed to create sampler" << std::endl;
+        return false;
+    }
+    
+    llama_sampler_chain_add(m_sampler, llama_sampler_init_greedy());
+    
+    return true;
+}
+
+void translate_engine::free_model()
+{
+    llama_sampler_free(m_sampler);
+    llama_model_free(m_model);
+}
+
+std::string translate_engine::translate_en_to_jp(const std::string& jp)
+{
+    std::stringstream ss;
+    ss << "<|plamo:op|>dataset\ntranslation\n<|plamo:op|>input lang=English\n" << jp <<"\n<|plamo:op|>output lang=Japanese";
+    std::string prompt = ss.str();
+    
+    return tranlate(prompt);
+}
+
+std::string translate_engine::translate_jp_to_en(const std::string& en)
+{
+    std::stringstream ss;
+    ss << "<|plamo:op|>dataset\ntranslation\n<|plamo:op|>input lang=Japanese\n" << en <<"\n<|plamo:op|>output lang=English";
+    std::string prompt = ss.str();
+    
+    return tranlate(en);
+}
+
+std::string translate_engine::tranlate(const std::string& prompt)
+{
+    const llama_vocab *vocab = llama_model_get_vocab(m_model);
+    const int n_prompt = -llama_tokenize(vocab, prompt.c_str(), prompt.size(), nullptr, 0, true, true);
+
+    std::vector<llama_token> prompt_tokens(n_prompt);
+    if (llama_tokenize(vocab, prompt.c_str(), prompt.size(), prompt_tokens.data(), prompt_tokens.size(), true, true) < 0)
+    {
+        std::cerr << "failed to tokenize prompt" << std::endl;
+        return "";
+    }
+
+    llama_context_params ctx_params = llama_context_default_params();
+    ctx_params.n_ctx = n_prompt + N_PREDICT - 1;
+    ctx_params.n_batch = n_prompt;
+    ctx_params.no_perf = false;
+
+    llama_context *ctx = llama_init_from_model(m_model, ctx_params);
+    if (ctx == nullptr)
+    {
+        std::cerr << "failed to init model" << std::endl;
+        return "";
+    }
+
+    llama_sampler_chain_params sampler_params = llama_sampler_chain_default_params();
+    sampler_params.no_perf = false;
+    llama_sampler *sampler = llama_sampler_chain_init(sampler_params);
+
+    llama_sampler_chain_add(sampler, llama_sampler_init_greedy());
+
+    for (llama_token token : prompt_tokens)
+    {
+        char buf[128];
+        int n = llama_token_to_piece(vocab, token, buf, sizeof(buf), 0, true);
+        if (n < 0)
+        {
+            std::cerr << "failed to convert token to piece" << std::endl;
+            return "";
+        }
+        std::string s(buf, n);
+        std::cout << s;
+    }
+
+    llama_batch batch = llama_batch_get_one(prompt_tokens.data(), prompt_tokens.size());
+
+    if (llama_model_has_encoder(m_model))
+    {
+        if (llama_encode(ctx, batch))
+        {
+            std::cerr << "failed to eval" << std::endl;
+            return "";
+        }
+
+        llama_token decoder_start_token_id = llama_model_decoder_start_token(m_model);
+        if (decoder_start_token_id == LLAMA_TOKEN_NULL)
+        {
+            decoder_start_token_id = llama_vocab_bos(vocab);
+        }
+
+        batch = llama_batch_get_one(&decoder_start_token_id, 1);
+    }
+
+    const int64_t main_start = ggml_time_us();
+    int n_decode = 0;
+    llama_token new_token_id;
+
+    std::stringstream result;
+
+    for (int n_pos = 0; n_pos + batch.n_tokens < n_prompt + N_PREDICT; )
+    {
+        if (llama_decode(ctx, batch))
+        {
+            std::cerr << "failed to decode" << std::endl;
+            return "";
+        }
+
+        n_pos += batch.n_tokens;
+
+        new_token_id = llama_sampler_sample(sampler, ctx, -1);
+
+        if (llama_vocab_is_eog(vocab, new_token_id))
+        {
+            break;
+        }
+
+        char buf[128];
+        int n = llama_token_to_piece(vocab, new_token_id, buf, sizeof(buf), 0, true);
+        if (n < 0)
+        {
+            std::cerr << "failed to convert token to piece" << std::endl;
+            return "";
+        }
+
+        std::string s(buf, n);
+        std::cout << s;
+        result << s;
+        fflush(stdout);
+
+        batch = llama_batch_get_one(&new_token_id, 1);
+
+        n_decode += 1;
+    }
+
+    std::cout << std::endl;
+
+    const int64_t main_end = ggml_time_us();
+
+    std::cout << "decoded " << n_decode << " tokens, " << static_cast<double>(main_end - main_start) / 1000000.0 << "s, " << static_cast<double>(n_decode) / (static_cast<double>(main_end - main_start) / 1000000) << " t/s" << std::endl;
+
+    std::cout << std::endl;
+    llama_perf_sampler_print(sampler);
+    llama_perf_context_print(ctx);
+    std::cout << std::endl;
+
+    llama_free(ctx);
+
+    return result.str();
+}
--- engine/translate_engine.h
+++ engine/translate_engine.h
@@ -11,15 +11,22 @@ public:
     translate_engine() = default;
     ~translate_engine();
 
-    void load_model();
+    bool load_model();
     void free_model();
 
-    std::string translate_en_to_jp(std::string jp);
-    std::string translate_jp_to_en(std::string en);
+    std::string translate_en_to_jp(const std::string& jp);
+    std::string translate_jp_to_en(const std::string& en);
 
 private:
+    std::string tranlate(const std::string& prompt);
+
     llama_model *m_model = nullptr;
     llama_sampler *m_sampler = nullptr;
+
+    static constexpr int N_PREDICT = 100;
+    static constexpr int N_GPU_LAYERS = 99;
+
+    const std::string MODEL_PATH = "./plamo-2-translate.gguf";
 };
 
 
